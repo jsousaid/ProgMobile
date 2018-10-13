85fa4f9105fdd63ee72b6e80300af8087c8bb3cd
Leonardo Coelho <leonardoabaque@gmail.com>
2018-10-12 23:55:22 -0300

    Criação da tela de agendamentos para inplementação dos requisitos.

--- Projeto/app/src/main/java/br/fatec/projeto/AgendamentoActivity.java
+++ Projeto/app/src/main/java/br/fatec/projeto/AgendamentoActivity.java
@@ -2,8 +2,23 @@ package br.fatec.projeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgendamentoActivity extends AppCompatActivity {
+
+    private List<Servico> servicosSelecionados = new ArrayList<>();
+    private RecyclerView recyclerServicos;
+    private TextView txtTotal;
+    private double valorTotal = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +28,49 @@ public class AgendamentoActivity extends AppCompatActivity {
         // Colocar setinha pra voltar na tela de menu
         getSupportActionBar().setTitle("Agendamento");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerServicos = (RecyclerView) findViewById(R.id.rcv_servicos_agendados);
+        RecyclerView.LayoutManager m = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL,
+                false);
+        recyclerServicos.setLayoutManager(m);
+
+        txtTotal = (TextView) findViewById(R.id.txt_total);
+
+        Button addServicoButton = (Button) findViewById(R.id.btn_add_servico);
+        addServicoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Spinner servicoSpinner = (Spinner) findViewById(R.id.spn_servicos);
+                String value = (String) servicoSpinner.getSelectedItem();
+
+                switch (value) {
+                    case "Adestramento":
+                        servicosSelecionados.add(new Servico("Adestramento", 32.5));
+                        break;
+                    case "Banho e Tosa":
+                        servicosSelecionados.add(new Servico("Banho e Tosa", 25));
+                        break;
+                    case "Consulta Veterinária":
+                        servicosSelecionados.add(new Servico("Consulta Veterinária", 48.90));
+                        break;
+                    case "Hospedagem":
+                        servicosSelecionados.add(new Servico("Hospedagem", 28.15));
+                        break;
+                }
+
+                recyclerServicos.setAdapter(new ServicoAdapter(servicosSelecionados, getApplicationContext()));
+
+                valorTotal = 0;
+
+                for (int i = 0; i < servicosSelecionados.size(); i++) {
+                    Servico servico = servicosSelecionados.get(i);
+
+                    valorTotal += servico.getPreco();
+                }
+
+                txtTotal.setText("Total: R$" + String.format("%.2f", valorTotal));
+            }
+        });
     }
 }
